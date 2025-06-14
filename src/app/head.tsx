ae79f1b63c228f371e636d1438d5d7c3ebdcb915
Arjun Jhukal <jhukal9@gmail.com>
2025-06-14 16:28:20 +0545

    updated the google ads

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+// app/head.tsx
+export default function Head() {
+    return (
+        <>
+            <meta name="google-adsense-account" content="ca-pub-8678344611307925" />
+        </>
+    );
+}
