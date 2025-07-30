745a74afafc3aa51b3caaa8c45bbf3eb0cb589fa
Arjun Jhukal <jhukal9@gmail.com>
2025-07-30 23:23:11 +0545

    updated the faq and highlight and quote block

--- src/utils/renderComponent/componentrenderer.tsx
+++ src/utils/renderComponent/componentrenderer.tsx
@@ -1,9 +1,10 @@
 import BlogDetail from "@/components/BlogDetail";
+import Faq from "@/components/Faq";
 import YoutubeIframe from "@/components/Iframe";
 import MomentGallery from "@/components/moment";
 import Quote from "@/components/Quote";
 import SingleImage from "@/components/SingleImage";
-import { BlogContentRecord, BlogDetailT, GalleryProps, IframeRecordType, QuoteRecordType, SingleImageBlockRecord } from "@/interface";
+import { BlogContentRecord, BlogDetailT, FaqProps, GalleryProps, IframeRecordType, QuoteRecordType, SingleImageBlockRecord } from "@/interface";
 
 export const RenderComponent = (section: BlogDetailT) => {
   switch (section.__typename) {
@@ -17,5 +18,7 @@ export const RenderComponent = (section: BlogDetailT) => {
       return <YoutubeIframe data={section as IframeRecordType} />;
     case "MomentGalleryRecord":
       return <MomentGallery data={section as GalleryProps} />;
+    case "FaqRecord":
+      return <Faq data={section as FaqProps} />;
   }
 };
