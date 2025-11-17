fd0c0ccbf6cd8be7fc58a3730cd3d618c7880798
Arjun Jhukal <jhukal9@gmail.com>
2025-11-17 20:25:50 +0545

    updated the new promotion banner for personal youtube channel at top banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata, Viewport } from "next";
 import { Mulish } from "next/font/google";
-import Link from "next/link";
 import Script from "next/script";
 import "./globals.css";
 
@@ -68,10 +67,10 @@ export default function RootLayout({
 				{process.env.NEXT_PUBLIC_GAID && (
 					<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID} />
 				)}
-				<Link target="_blank" href={"https://www.youtube.com/watch?v=k5gUdTxqq38"} className="w-full block bg-primary text-white text-center p-2 sticky top-0 right-0 left-0 z-10">
+				{/* <Link target="_blank" href={"https://www.youtube.com/watch?v=k5gUdTxqq38"} className="w-full block bg-primary text-white text-center p-2 sticky top-0 right-0 left-0 z-10">
 					<div className="">
 						<span className="underline text-red-600">Click</span> to Join us on our Youtube Channel. </div>
-				</Link>
+				</Link> */}
 				<Header />
 				<main className="main">{children}</main>
 				<Footer />
